79591c7920856a69cbe967b1f0192d3b79ee03d9
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-04 17:34:12 +0700

    Add files via upload

---
+++ asset/Map/Outside_A4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Outside_A4" tilewidth="30" tileheight="30" tilecount="600" columns="25">
+ <image source="Outside_A4.png" width="768" height="720"/>
+</tileset>
