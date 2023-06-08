32fd197928a9149ed5f3132fae9014f1d1d63729
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-08 08:44:05 +0700

    Add files via upload

---
+++ asset/Map/SF_Outside_C.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="SF_Outside_C" tilewidth="1" tileheight="1" spacing="102" margin="55" tilecount="49" columns="7">
+ <image source="SF_Outside_C.png" width="768" height="768"/>
+</tileset>
