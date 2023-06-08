4cee06d36d4478e8338337a6f3e39dd8fe742fc8
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-08 08:39:33 +0700

    Update and rename object.py to character.py

--- character.py
+++ character.py
@@ -2,7 +2,10 @@ import pygame
 from load_image import *
 from load_music import *
 from projectile import *
+from load_map import *
 
+
+#Tạo đối tượng nhân vật
 class player(object):
     
     def __init__(self,x,y,width,height):
@@ -10,15 +13,16 @@ class player(object):
         self.y = y
         self.width = width
         self.height = height
-        self.vel = 5
+        self.vel = 10
         self.last_dash_time = 0
         self.dash_time = 500 
         self.isJump = False
         self.isDash = False
+        self.isFalling = True
         self.left = False
         self.right = True
         self.walkCount = 0
-        self.jumpCount = 10
+        self.jumpCount = 9
         self.standing = True
         self.isAttack = False
         self.isAttacking = False
@@ -26,10 +30,11 @@ class player(object):
         self.current_time = 0
         self.last_attack_time = 0
         self.attack_interval = 150
-        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
+        self.hitbox = (self.x + 6 , self.y , 29, 52)
         self.prev_action = "right"
         self.now_action = "right"
         self.melee = AttackMelee(self.x,self.y)
+        self.gravity = (self.jumpCount ** 2) * 0.5 * 1
     def get_prev_action(self):
         return self.prev_action
     
@@ -133,24 +138,36 @@ class player(object):
             self.walkCount = 0
     
     def handle_jump(self,keys):
-        if not(self.isJump):
+        
+        if not(self.isJump ) :
             if keys[pygame.K_x]:
                 channel_move.play(jump_sound)
                 self.isJump = True
                 self.right = False
                 self.left = False
                 self.walkCount = 0
+
+                
         else:
-            if self.jumpCount >= -10:
+            if self.jumpCount >= -9:
                 neg = 1
-                if self.jumpCount < 0:
-                    neg = -1
                 self.y -= (self.jumpCount ** 2) * 0.5 * neg
+                if self.jumpCount < 0:
+                    self.isJump = False
+                    self.isFalling = True
+
                 self.jumpCount -= 1
-            else:
-                self.isJump = False
-                self.jumpCount = 10
     
+            else:
+                self.isFalling = False
+                self.isJump = False
+                self.standing = True
+                self.jumpCount = 9
+                
+                
+
+        
+
     def handle_dash(self,keys):    
         if not(self.isDash):
             if keys[pygame.K_z]:
@@ -159,7 +176,7 @@ class player(object):
                 self.vel += 40
         else:
             self.isDash = False
-            self.vel = 5
+            self.vel = 10
      
     def handle_attack(self,keys):
         if keys[pygame.K_c]:
@@ -169,87 +186,28 @@ class player(object):
         else:
             self.isAttack = False
   
-
-
-
-
-
-class enemy(object):
-    def __init__(self, x, y, width, height, end):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.end = end
-        self.path = [self.x, self.end]
-        self.walkCount = 0
-        self.ishitted = False
-
-        self.vel = 2
-        self.hitbox = (self.x , self.y + 2, 20, 30)
-        self.health = 10
-        self.visible = True
-        self.current_time = 0
-        self.last_hitted = 0
-        self.hitted_duration = 200
-        self.action = "right"
-    def draw(self,win):
-        self.move()
-        if self.visible:
-            if self.walkCount + 1 >= 33:
-                self.walkCount = 0
-
-            if self.action == "right":
-                
-                    win.blit(walkRightE1[self.walkCount %3], (self.x, self.y))
-                    self.walkCount += 1
-            elif self.action == "left":
-                
-                    win.blit(walkLeftE1[self.walkCount %3], (self.x, self.y))
-                    self.walkCount += 1
-    
-                
-            
-            
-            pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
-            pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10))
-            self.hitbox = (self.x +5, self.y + 2, 31, 50)
-            #pygame.draw.rect(win, (255,0,0), self.hitbox,2)
-            
-            if self.ishitted:
-                self.vel = 0
-            else:
-                self.vel = 2
-                
-
-                
+    def apply_gravity(self, map):
+        level_data = map.get_level_data("Wall")
         
+        if self.isJump:
+            return 
+        if self.isFalling:
+            hitbox = self.hitbox
 
-    def move(self):
-        
-        if self.action == "right":
-            if self.x + self.vel < self.path[1]:
-                self.x += self.vel
-            else:
-                self.action = "left"
-                self.walkCount = 0
-        elif self.action == "left":
-            if self.x - self.vel > self.path[0]:
-                    self.x -= self.vel
-            else:
-                self.action ="right"
-                self.walkCount = 0
-        
-    def hit(self):
-        self.ishitted = True
-        if self.ishitted:
-            if self.action == 'right' :
-                    self.action = 'left'
-            elif self.action == 'left':
-                    self.action ='right'
-        if self.health > 0:
-            self.health -=0.1    
-        else:
-            self.visible = False
-        
-        print('hit')
+            for row in range(len(level_data)):  
+                for col in range(len(level_data[row])):
+                    if level_data[row][col] != 0:
+                        tile_x = col * map.tmxdata.tilewidth
+                        tile_y = row * map.tmxdata.tileheight
+                        tile_rect = pygame.Rect(tile_x, tile_y- map.tmxdata.tileheight, map.tmxdata.tilewidth, map.tmxdata.tileheight)
+                        if tile_rect.colliderect(hitbox):
+                            self.isJump = False  
+                            self.jumpCount = 9
+                            self.y = tile_y - self.height + 15 
+                            return
+
+            self.y += self.gravity
+
+
+
+
