bee05351e428bdb156c2bd09eca8e20fda7cd274
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-14 21:14:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from enemy import Enemy
 from projectile import *
 from load_image import *
 from load_music import *
+from data_map import *
 from load_map import Map
 
 WIDTH, HEIGHT = 1080, 720
 
-
+#Khởi tạo
 pygame.init()
 pygame.font.init()
-pygame.mixer.init()
+
+
 # Thông tin game
 TITLE = "Hiệp sĩ Offline"
 MAX_FPS = 27
@@ -26,12 +28,11 @@ YELLOW  = (255, 160, 0)
 WIDTH, HEIGHT = 1080, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
-
-
-
+font =  pygame.font.Font(os.path.join('asset','Font',"dejavusans-boldoblique.ttf"), 20)
 selected_button = 0
 # Biến kiểm soát phần menu
 main_menu = True
+game_over = False
 button_new = Button(860, 290, 200, 50, "Chơi mới")
 button_continue = Button(860, 357, 200, 50, "Tiếp tục")
 button_load = Button(860, 424, 200, 50, "Tải")
@@ -39,24 +40,9 @@ button_setting = Button(860, 491, 200, 50, "Cài đặt")
 button_exit = Button(860, 558, 200, 50, "Thoát game")
 buttons = [button_new, button_continue, button_load, button_setting, button_exit]
 
-# Khởi tạo đối tượng
-man = player(100, 600, 64, 64)
-melee = AttackMelee(man.x,man.y)
-slime = Enemy(500 , 600 , 64, 64, 1000)
 
-#Âm thanh nền
-def main_sound():
-    if main_menu:
-        if channel_menu.get_busy():
-            return
-        else:
-            channel_menu.play(main_menu_music)
-    else:
-        channel_menu.stop()
-        if channel_game.get_busy():
-            return
-        else:
-            channel_game.play(game_music)
+
+
 #Vẽ menu
 def draw_menu():
     WIN.blit(main_menu_img, (0, 0))
@@ -67,25 +53,28 @@ def draw_menu():
         else:
             button.draw_black(WIN)
     pygame.display.update()
+    
+    
+
 #Tương tác với menu
 def select_menu(event):
-    global selected_button
+    global selected_button, run
 
     if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                select_bar_sound.play()
+                channel_select_bar.play(select_bar_sound)
                 if selected_button == 0:
                     selected_button = len(buttons) - 1
                 else:
                     selected_button -= 1
             elif event.key == pygame.K_DOWN:
-                select_bar_sound.play()
+                channel_select_bar.play(select_bar_sound)
                 if selected_button == len(buttons) -1:
                     selected_button = 0
                 else:
                     selected_button += 1
             elif event.key == pygame.K_RETURN:
-                accept_sound.play()
+                channel_select_bar.play(accept_sound)
                 if selected_button == 0:
                     start_game()
                 elif selected_button == 1:
@@ -95,46 +84,89 @@ def select_menu(event):
                 elif selected_button == 3:
                     print('Cài đặt')
                 elif selected_button == 4:
-                    pygame.quit()
+                    run = False
 
 
-def playerattack():
-    if man.melee.hitbox[1] - man.melee.hitbox[3] < slime.hitbox[1] + slime.hitbox[3] and man.melee.hitbox[1] + man.melee.hitbox[3] > slime.hitbox[1]:
-        if man.isAttack and man.prev_action == "right":
-            if (man.melee.hitbox[0] + man.melee.hitbox[2]) > slime.hitbox[0] and man.melee.hitbox[0] < slime.hitbox[0] + slime.hitbox[2]:
-                slime.hit() 
-        elif man.isAttack and man.prev_action == "left":
-            if (man.melee.hitbox[0] - man.melee.hitbox[2]) < slime.hitbox[0]  and man.melee.hitbox[0] > slime.hitbox[0] :
-                slime.hit() 
-        else:
-            slime.ishitted = False
+#Xử lý các đòn đánh
+def playerattack(enemies):
+    for enemy in enemies:
+        if man.melee.hitbox[1] - man.melee.hitbox[3] < enemy.hitbox[1] + enemy.hitbox[3] and man.melee.hitbox[1] + man.melee.hitbox[3] > enemy.hitbox[1]:
+            if man.isAttack and man.prev_action == "right":
+                if (man.melee.hitbox[0] + man.melee.hitbox[2]) > enemy.hitbox[0] and man.melee.hitbox[0] < enemy.hitbox[0] + enemy.hitbox[2]:
+                    enemy.hit() 
+            elif man.isAttack and man.prev_action == "left":
+                if (man.melee.hitbox[0] - man.melee.hitbox[2]) < enemy.hitbox[0]  and man.melee.hitbox[0] > enemy.hitbox[0] :
+                    enemy.hit() 
+            else:
+                enemy.ishitted = False
+#Xử lý khi bị tấn công
+def playerhitted(enemies, man, WIN):
+    for enemy in enemies:
+        if enemy.visible == True:
+            if man.hitbox[1] < enemy.hitbox[1] + enemy.hitbox[3] and man.hitbox[1] + man.hitbox[3] > enemy.hitbox[1]:
+                if (man.hitbox[0] + man.hitbox[2]) > enemy.hitbox[0] and man.hitbox[0] < enemy.hitbox[0] + enemy.hitbox[2]:
+                        man.hit(WIN) 
+                    
+#Thông tin các Màn 
+map_0 = "asset/Map/Map_tutorial.tmx"
+map_1 = "asset/Map/Map_1.tmx"
+map_2 = "asset/Map/Map_2.tmx"
+map_3 = "asset/Map/Map_3.tmx"
+map_4 = "asset/Map/Map_4.tmx"
+lstmap= [map_0,map_1,map_2,map_3,map_4]
+index_map = 0
+enemies = []
+man = player(0,0,0,0)
 
-map_filename = "asset/Map/Map_tutorial.tmx"
-game_map = Map(map_filename)
-map_now = game_map
-# Vẽ bản đồ
-map_surface = game_map.make_map()
 
+# Vẽ các màn chơi
+def draw_map(map_filename):
+    global enemies, man
+    game_map = Map(map_filename)
+    map_now = game_map
+    map_surface = game_map.make_map(bg[index_map])
+    enemies = draw_enemy(map_now, WIN)
+    man = draw_player(map_now, WIN)
+    return map_now, map_surface
+
+map_inf = draw_map(lstmap[index_map])
 
 #vẽ game
-def draw_game():  
-    WIN.blit(map_surface, (0,0))
+def draw_game(keys):  
+    global main_menu, game_over,index_map, map_inf
+    WIN.blit(map_inf[1], (0,0))
     man.draw(WIN)
-    slime.draw(WIN)
+    for enemy in enemies:
+        enemy.draw(WIN, map_inf[0])
+        
+    if keys[pygame.K_ESCAPE]:
+        main_menu = True
+        game_over = False
+        draw_map(lstmap[index_map])
+        draw_menu()
+    if man.health <= 0:
+        WIN.blit(game_over_img, (0,0))
+        text = "Nhấn ESC để về màn hình chính"
+        text_render  = font.render(text, True, WHITE)
+        WIN.blit(text_render, (350, 500))
+        game_over = True
+    if man.progress =="clear":
+        index_map += 1
+        man.progress = 0
+        map_inf = draw_map(lstmap[index_map])
     pygame.display.update()
  
-
-# Vòng lặp game
+#Khởi tạo trò chơi
 def start_game():
     global main_menu
     main_menu = False
-    draw_game()
-
-def main():
-    clock = pygame.time.Clock()
-    run = True    
-    while run:
-        main_sound()
+    
+    
+# Vòng lặp game
+run = True   
+clock = pygame.time.Clock()
+while run == True:
+        main_sound(main_menu, game_over)
         keys = pygame.key.get_pressed()
         clock.tick(MAX_FPS)
         for event in pygame.event.get():
@@ -145,13 +177,10 @@ def main():
                 draw_menu()   
                 select_menu(event)
         if not main_menu:
-            draw_game()
-            man.move(keys)
-            man.apply_gravity(map_now)   
-            playerattack()
-    pygame.quit() 
-
-
-if __name__ == "__main__":
-    main()
+            draw_game(keys)
+            man.move(keys, map_inf[0])
+            playerattack(enemies)
+            playerhitted(enemies, man,WIN)
+            
+pygame.quit() 
 
