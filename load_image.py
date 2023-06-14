5efeb9772656ee209498837baf651b4edd63c821
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-14 20:59:54 +0700

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -1,28 +1,31 @@
 import pygame
 import os
 
+WIDTH, HEIGHT = 1080, 720
+
+
+#Các hàm để xử lý ảnh 
 def load_image(type, name):
     image = pygame.image.load(os.path.join('asset', type ,name))
     return image
     
-
 def transform(image, width, height):
     image = pygame.transform.scale(image,(width, height))
     return image
-
 
 def flip(image, horizontal , vertical):
     image = pygame.transform.flip(image,horizontal,vertical)
     return image
 
  
-WIDTH, HEIGHT = 1080, 720
 # Load hình nền
 main_menu_img = load_image('bg','WorldMap.png')
 main_menu_img = transform(main_menu_img,WIDTH,HEIGHT)
 select_bar = load_image('bg',"Selectbar.png")
-bg = load_image('bg','Tower1.png')
-bg = transform(bg,WIDTH,HEIGHT)
+bg = [load_image('bg','Tower1.png'), load_image('bg','Night.png'),load_image('bg','Castle.png'),load_image('bg','DemonCastle.png'),load_image('bg','Devil.png')]
+bg = [transform(bg_index,WIDTH,HEIGHT) for bg_index in bg]
+game_over_img = load_image('bg','GameOver.png')
+game_over_img = transform(game_over_img,WIDTH,HEIGHT)
 
 #Load chuyển động nhân vật
 walkRight = [load_image('character','R1.png'), load_image('character','R2.png'), load_image('character','R3.png')]
