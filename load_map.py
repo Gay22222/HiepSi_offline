4a116f1166d4495d2a998699749b22be87eaf4e6
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-14 21:02:03 +0700

    Update load_map.py

--- load_map.py
+++ load_map.py
@@ -6,33 +6,15 @@ from enemy import Enemy
 
 #Tạo đối tượng Map
 class Map:
+    #Khởi tạo
     def __init__(self, filename):
         tm = pytmx.load_pygame(filename)
         self.width = tm.width * tm.tilewidth
         self.height = tm.height * tm.tileheight
         self.tmxdata = tm
-        self.enemies = []
-    
-    def make_enemy(self):
-        objectgroup = None
-        for layer in self.tmxdata.layers:
-            if isinstance(layer, pytmx.TiledObjectGroup) and layer.name == "enemy1":
-                objectgroup = layer
-                break
-
-        if objectgroup:
-            for obj in objectgroup:
-                x = obj.x
-                y = obj.y- 13
-                width = obj.width
-                height = obj.height
-                end =  self.tmxdata.tilewidth
-                enemy = Enemy(x, y, width, height, end)  
-                self.enemies.append(enemy) 
-      
-      
-      
         
+      
+    #Lấy thông tin layer từ file tmx
     def get_level_data(self, layer_name):
         level_data = []
         layer = self.tmxdata.get_layer_by_name(layer_name)
@@ -45,7 +27,7 @@ class Map:
                 level_data.append(row)
         return level_data
 
-    
+    #Xử lý bản đồ
     def render(self, surface):
         ti = self.tmxdata.get_tile_image_by_gid
         for layer in self.tmxdata.visible_layers:
@@ -55,17 +37,12 @@ class Map:
                         tile = ti(gid)
                         if tile:
                             surface.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))
-
-    def make_map(self):
+    #Vẽ bản đò
+    def make_map(self, bg):
         background_surface = pygame.Surface((self.width, self.height))
         background_image = bg 
         background_surface.blit(bg, (0, 0))
         self.render(background_surface)
-        # self.make_enemy()
-        # for enemy in self.enemies:
-        #     enemy.update()
-        #     enemy.draw(background_surface)
-
         return background_surface
 
 
