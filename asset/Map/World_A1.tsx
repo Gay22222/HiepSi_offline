c6da80637969689a793684bb43670fff9b853982
Gay22222 <126012413+Gay22222@users.noreply.github.com>
2023-06-14 20:51:08 +0700

    Delete asset/Map directory

--- asset/Map/World_A1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="World_A1" tilewidth="30" tileheight="30" tilecount="475" columns="25">
- <image source="World_A1.png" width="768" height="576"/>
-</tileset>
